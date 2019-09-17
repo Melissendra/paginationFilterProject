1fab1ace8e1b05b7faae273e1adb93fa7cdc09df
Melissendra <melissendra77@gmail.com>
2019-09-17 16:16:17 +0200

    error message ok

--- js/script.js
+++ js/script.js
@@ -1,146 +1,130 @@
-// we create this event listener so we don't care where is the script tag link in the html file
-document.addEventListener("DOMContentLoaded", () => {
-    // global variables for the project
-    const studentList = document.querySelectorAll(".student-item");
-    const nbrItems = 10;
-    const page = document.querySelector('.page');
 
-    //function to create elements
-    const createElement = (elementName, prop, value) => {
-        const element = document.createElement(elementName);
-        element[prop] = value;
-        return element;
-    };
+// global variables for the project
+const studentList = document.querySelectorAll(".student-item");
+const nbrItems = 10;
+const page = document.querySelector('.page');
 
-    //Function to determine how many students we want per page
-    const showPage = (list, page) => {
-        const startIndex = (page * nbrItems) - nbrItems;
-        const endIndex = page * nbrItems;
+//function to create elements
+const createElement = (elementName, prop, value) => {
+    const element = document.createElement(elementName);
+    element[prop] = value;
+    return element;
+};
 
-        for(let i = 0; i < list.length; i++){
-            if(i >= startIndex && i < endIndex){
-                list[i].style.display = '';
-            }else {
-                list[i].style.display = 'none';
-            }
+// Creation of the search input
+
+
+//Function to determine how many students we want per page
+const showPage = (list, page) => {
+    const startIndex = (page * nbrItems) - nbrItems;
+    const endIndex = page * nbrItems;
+
+    for(let i = 0; i < list.length; i++){
+        if(i >= startIndex && i < endIndex){
+            list[i].style.display = '';
+        }else {
+            list[i].style.display = 'none';
         }
-    };
+    }
+};
 
-    //function creating pages buttons dynamically
-    const appendPageLinks = (list) => {
-        const totalPages = Math.ceil(studentList.length / nbrItems);
-        const pageDiv = createElement('div', 'className', 'pagination');
-        const pageUl = createElement('ul');
+showPage(studentList, 1)
 
-        //loop to create dynamically our pages links
-        for(let i = 0; i < totalPages; i++){
-            let pageNumber = i + 1;
-            const pageLi = createElement('li');
-            const pageLink = createElement('a', 'textContent', pageNumber);
+//function creating pages buttons dynamically
+const appendPageLinks = (list) => {
+    const totalPages = Math.ceil(studentList.length / nbrItems);
+    const pageDiv = createElement('div', 'className', 'pagination');
+    const pageUl = createElement('ul');
 
-            if(pageNumber === 1){
-                pageLink.className = 'active';
-            }
+    const pagination = document.querySelector(".pagination");
 
-            //eventlistener to go in the other pages and active the right links when clicked
-            pageLink.addEventListener('click', (e)=> {
-                const clickedLink = e.target;
-                const nbrPage = parseInt(clickedLink.textContent);
-                const activeLink = document.querySelector('.active');
-
-                activeLink.className = '';
-                clickedLink.className = 'active';
-
-                showPage(studentList, nbrPage);
-            });
-            
-            pageLi.appendChild(pageLink);
-            pageUl.appendChild(pageLi);
-        }
-        pageDiv.appendChild(pageUl);
-        page.appendChild(pageDiv);
-    };
-
-    // Creation of the search input
-    const searchForm = () => {
-        const header = document.querySelector(".page-header");
-        const searchDiv = createElement('div', 'className', 'student-search');
-        const searchInput = createElement('input', 'type', 'text');
-        searchInput.placeholder = "Search for students...";
-        const searchButton = createElement('button', 'textContent', 'search');
-
-
-        searchDiv.appendChild(searchInput);
-        searchDiv.appendChild(searchButton);
-        header.appendChild(searchDiv);
-
-        searchButton.addEventListener('click', () =>{
-            search(searchInput, studentList);
-        });
-
-        searchInput.addEventListener("keyup", () =>{
-            search(searchInput, studentList);
-        });
-
+    //we check if the div with a class pagination exits
+    if(pagination){
+        pagination.parentNode.removeChild(pagination);
     }
 
-    const search = (search, student) => {
-        const userInput = search.value.toLowerCase();
-        let results = [];
+    //loop to create dynamically our pages links
+    for(let i = 0; i < totalPages; i++){
+        let pageNumber = i + 1;
+        const pageLi = createElement('li');
+        const pageLink = createElement('a', 'textContent', pageNumber);
 
-        removePageLinks();
-
-        if(!search.value){
-            restore(studentList);
+        if(pageNumber === 1){
+            pageLink.className = 'active';
         }
 
-        for(let i = 0; i < student.length; i++){
-            const listResult = student[i].querySelector("h3");
-            const textValue = listResult.textContent.toLowerCase();
-            if(textValue.includes(userInput)){
-                student[i].style.display = '';
-                results.push(student[i]);
-            }else{
-                student[i].style.display ='none';
-            }
-        }
+        //event listener to go in the other pages and active the right links when clicked
+        pageLink.addEventListener('click', (e)=> {
+            const clickedLink = e.target;
+            const nbrPage = parseInt(clickedLink.textContent);
+            const activeLink = document.querySelector('.active');
 
-        if(results.length === 0 || userInput === ''){
-            const noResult = createElement("span", "textContent", "");
-            noResult.textContent = "No result found";
-            page.appendChild(noResult);
-        }else if(results.length <=  10){
-            showPage(results, 1);
+            activeLink.className = '';
+            clickedLink.className = 'active';
+
+            showPage(studentList,nbrPage);
+        });
+        
+        pageLi.appendChild(pageLink);
+        pageUl.appendChild(pageLi);
+    }
+    pageDiv.appendChild(pageUl);
+    page.appendChild(pageDiv);
+};
+
+appendPageLinks(studentList);
+
+const searchForm = () => {
+    const header = document.querySelector(".page-header");
+    const searchDiv = createElement('div', 'className', 'student-search');
+    const searchInput = createElement('input', 'type', 'text');
+    searchInput.placeholder = "Search for students...";
+    const searchButton = createElement('button', 'textContent', 'search');
+
+    const searchDivClass = document.querySelector(".student-search");
+    if(searchDivClass){
+        searchDivClass.parentNode.removeChild(searchDivClass);
+    }
+
+    searchDiv.appendChild(searchInput);
+    searchDiv.appendChild(searchButton);
+    header.appendChild(searchDiv);
+
+    searchButton.addEventListener('click', () =>{
+        search(searchInput, studentList);
+    });
+
+    searchInput.addEventListener("keyup", () =>{
+        search(searchInput, studentList);
+    });
+}
+
+searchForm();
+
+const search = (search, student) => {
+    const userInput = search.value.toLowerCase();
+    let results = [];
+
+    for(let i = 0; i < student.length; i++){
+        const name = student[i].textContent.toLowerCase();
+
+        if(userInput !== 0 && name.includes(userInput)){
+            results.push(student[i]);
         }else{
-            showPage(results, 1);
-            appendPageLinks(results);
+            student[i].style.display ='none';
         }
-
-        restore(studentList);
     }
 
-    const restore = (results) => {
-        const link = document.querySelector('.pagination');
-        page.removeChild(link);
-        showPage(results,1);
+    if(results.length === 0 && userInput.length !== 0){
+        const noResult = createElement("span", "className", "error");
+        const error = document.querySelector(".error");
+        if(error){
+            error.parentNode.removeChild(error);
+        }
+        noResult.textContent = "No result found";
+        page.appendChild(noResult);
+    }else {
+        showPage(results, 1);
         appendPageLinks(results);
     }
-
-    const removePageLinks = () => {
-        const pageLinks = document.querySelector('.pagination');
-        if(pageLinks){
-            const parent = pageLinks.parentNode;
-            parent.removeChild(pageLinks);
-        }
-    }
-
-
-    showPage(studentList,1);
-    searchForm();
-    appendPageLinks(studentList);
-});
-
-
-
-
-
+}
